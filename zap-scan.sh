160b9e55a8dcb7db3954a3e5390f60e40a285385
joakoo123 <beltranlive9@gmail.com>
2025-11-26 22:45:09 -0300

    Fix: use zap2docker-weekly (stable image removed)

--- zap-scan.sh
+++ zap-scan.sh
@@ -2,11 +2,11 @@
 
 echo "Running OWASP ZAP Baseline Scan..."
 
-docker pull owasp/zap2docker-stable
+docker pull owasp/zap2docker-weekly
 
 docker run --rm \
   -v $(pwd)/reports/zap:/zap/reports \
-  owasp/zap2docker-stable \
+  owasp/zap2docker-weekly \
   zap-baseline.py \
   -t http://host.docker.internal:5000 \
   -r zap-report.html
